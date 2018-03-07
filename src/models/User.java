5d91054d296113c27ffaf16b245f4db488f4b791
lbroll <225972@student.pwr.edu.pl>
2018-03-07 09:43:26 +0100

    Resolved discussions

--- src/models/User.java
+++ src/models/User.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 /**
  * Abstract class of a player.
  */
-public abstract class User {
+final public class User {
 	/**
 	 * An id of Player.
 	 */
@@ -25,7 +25,6 @@ public abstract class User {
 	/**
 	 * Player's deck of Cards.
 	 */
-	private ArrayList<Card> cardsDeck;
 
 	public User() {};
 
@@ -35,7 +34,6 @@ public abstract class User {
 	 * @param score
 	 */
 	public User(int id, String name, String score) {
-		super();
 		this.id = id;
 		this.name = name;
 		this.score = score;
@@ -94,19 +92,7 @@ public abstract class User {
 	public void setColor(String color) {
 		this.color = color;
 	}
-	/**
-	 * @return the cardsDeck
-	 */
-	public ArrayList<Card> getCardsDeck() {
-		return cardsDeck;
-	}
 
-	/**
-	 * @param cardsDeck the cardsDeck to set
-	 */
-	public void setCardsDeck(ArrayList<Card> cardsDeck) {
-		this.cardsDeck = cardsDeck;
-	}
 	/**
 	 * Play card on board.
 	 */
@@ -118,48 +104,9 @@ public abstract class User {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((cardsDeck == null) ? 0 : cardsDeck.hashCode());
-		result = prime * result + id;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((score == null) ? 0 : score.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		if (cardsDeck == null) {
-			if (other.cardsDeck != null)
-				return false;
-		} else if (!cardsDeck.equals(other.cardsDeck))
-			return false;
-		if (id != other.id)
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (score == null) {
-			if (other.score != null)
-				return false;
-		} else if (!score.equals(other.score))
-			return false;
-		return true;
-	}
-
-	@Override
 	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", score=" + score + ", cardsDeck=" + cardsDeck + "]";
+		return "User [id=" + id + ", name=" + name + ", score=" + score + ", color=" + color + "]";
 	}
+
 
 }
