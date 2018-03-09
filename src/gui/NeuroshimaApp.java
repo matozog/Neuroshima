8a7d0fae608c4a7d1e11746b2bcc73ebf14691ba
226109@student.pwr.edu.pl <226109@student.pwr.edu.pl>
2018-03-09 01:36:44 +0100

    Improved board view

--- src/gui/NeuroshimaApp.java
+++ src/gui/NeuroshimaApp.java
@@ -25,7 +25,10 @@ import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
-import java.awt.Color;
+import java.awt.Color; 
+import javax.swing.border.EtchedBorder;
+import java.awt.Font;
+import javax.swing.SwingConstants;
 
 public class NeuroshimaApp implements ActionListener, MenuListener {
 
@@ -34,8 +37,13 @@ public class NeuroshimaApp implements ActionListener, MenuListener {
 	private JMenuItem mntmExit;
 	private JMenu mnAbout;
 	private JMenu mnHelp;
+	private JPanel panelGameMain;
 	private ArrayList<JPanel> playersPanels = new ArrayList<JPanel>();
-	private ArrayList<JLabel> playersLabel = new ArrayList<JLabel>(); 
+	private ArrayList<JLabel> playersLabel = new ArrayList<JLabel>();
+	private ArrayList<JLabel> boardCells = new ArrayList<JLabel>();
+	private JLabel lblNextTurn;
+	private JPanel panelNextTurn;
+	
 
 	/**
 	 * Create the application.
@@ -50,11 +58,24 @@ public class NeuroshimaApp implements ActionListener, MenuListener {
 		frame.getContentPane().setLayout(null);
 		frame.setContentPane(new JLabel(new ImageIcon(getClass().getResource("/gui/images/grass_texture.jpg"))));
 
-		JPanel panelGameMain = new JPanel();
-		panelGameMain.setBorder(BorderFactory.createTitledBorder("Game board"));
-		panelGameMain.setBounds(305, 62, 424, 411);
+		panelGameMain = new JPanel();
+		panelGameMain.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		panelGameMain.setBounds(305, 62, 424, 412);
 		frame.getContentPane().add(panelGameMain);
+		panelGameMain.setLayout(null);
 		
+		panelNextTurn = new JPanel();
+		panelNextTurn.setBounds(305, 485, 424, 46);
+		panelNextTurn.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panelNextTurn.setLayout(null);
+		frame.getContentPane().add(panelNextTurn);
+		
+		lblNextTurn = new JLabel("<html>Next turn: <b>Player1</b>");
+		lblNextTurn.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNextTurn.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNextTurn.setBounds(10, 11, 404, 24);
+		panelNextTurn.add(lblNextTurn); 
+
 		JMenuBar menuBar = new JMenuBar();
 		frame.setJMenuBar(menuBar);
 
@@ -81,16 +102,16 @@ public class NeuroshimaApp implements ActionListener, MenuListener {
 
 		logWindow = new LogWindow();
 		logWindow.setVisible(true);
-		
+
 		GeneratePlayersPanels(4);
-		SetPlayerScore(3,100);
-
-
+		GenerateBoard();
+		SetPlayerScore(3, 100);
 
 	}
 
 	/**
 	 * Generate player panels
+	 * 
 	 * @param playersCount
 	 */
 	public void GeneratePlayersPanels(int playersCount) {
@@ -108,47 +129,74 @@ public class NeuroshimaApp implements ActionListener, MenuListener {
 		for (int i = 0; i < playersCount; i++) {
 			JPanel playerPanel = new JPanel();
 			JLabel scoreLabel = new JLabel("Score: 0");
-			
-			scoreLabel.setBounds(new Rectangle(8,15, 100, 20));
-			
+
+			scoreLabel.setBounds(new Rectangle(8, 15, 100, 20));
+
 			playerPanel.setLayout(null);
 			playerPanel.add(scoreLabel);
 			playerPanel.setBounds(bounds[i]);
-			playerPanel.setBorder(BorderFactory.createTitledBorder("Player #" + (i+1)));
-			
+			playerPanel.setBorder(BorderFactory.createTitledBorder("Player #" + (i + 1)));
+
 			try {
-				Image img1 = ImageIO.read(getClass().getResource("/gui/images/card"+(i+1)+".png"));
-				JLabel card1 = new JLabel();  
+				Image img1 = ImageIO.read(getClass().getResource("/gui/images/card" + (i + 1) + ".png"));
+				JLabel card1 = new JLabel();
 				card1.setIcon(logWindow.scaleImage(img1, 90, 130));
-				card1.setBounds(8,40,90,130);
+				card1.setBounds(8, 40, 90, 130);
 				playerPanel.add(card1);
-				
-				JLabel card2 = new JLabel();  
+
+				JLabel card2 = new JLabel();
 				card2.setIcon(logWindow.scaleImage(img1, 90, 130));
-				card2.setBounds(103,40,90,130);
+				card2.setBounds(103, 40, 90, 130);
 				playerPanel.add(card2);
-				
-				JLabel card3 = new JLabel();  
+
+				JLabel card3 = new JLabel();
 				card3.setIcon(logWindow.scaleImage(img1, 90, 130));
-				card3.setBounds(198,40,90,130);
+				card3.setBounds(198, 40, 90, 130);
 				playerPanel.add(card3);
-			} catch (IOException e) { 
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			
-			
+
 			playersLabel.add(scoreLabel);
 			playersPanels.add(playerPanel);
 			frame.getContentPane().add(playerPanel);
 		}
 	}
 	
+	public void SetNextTurn(int player) { 
+		lblNextTurn.setText("<html>Next turn: <b>Player"+player+"</b>");
+	}
+
+	/**
+	 * Generate game board
+	 */
+	public void GenerateBoard() {
+		int width = panelGameMain.getWidth();
+		int height = panelGameMain.getHeight();
+
+		int cellWidth = width / 4;
+		int cellHeight = height / 4; 
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				JLabel cell = new JLabel();
+				cell.setBounds(i*cellWidth,j*cellHeight,cellWidth,cellHeight);
+				cell.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+				boardCells.add(cell);
+				panelGameMain.add(cell);
+			}
+		}
+	}
+
 	/**
 	 * Set player score in label
-	 * @param playerId - player id indexing from 0 to 3
-	 * @param score - player overall score
+	 * 
+	 * @param playerId
+	 *            - player id indexing from 0 to 3
+	 * @param score
+	 *            - player overall score
 	 */
-	public void SetPlayerScore(int playerId, int score) { 
+	public void SetPlayerScore(int playerId, int score) {
 		playersLabel.get(playerId).setText("Score: " + score);
 	}
 
@@ -190,12 +238,12 @@ public class NeuroshimaApp implements ActionListener, MenuListener {
 	}
 
 	@Override
-	public void menuDeselected(MenuEvent e) { 
+	public void menuDeselected(MenuEvent e) {
 
 	}
 
 	@Override
-	public void menuCanceled(MenuEvent e) { 
+	public void menuCanceled(MenuEvent e) {
 
 	}
 }
